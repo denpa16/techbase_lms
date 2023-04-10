e12c786a441a8a2666a979f4fc4ce557036ff136
d.pavlov <d.pavlov@idaproject.com>
2023-04-10 15:22:13 +0300

    feat(courses): add course details

--- backend/courses/serializers/course_serializer.py
+++ backend/courses/serializers/course_serializer.py
@@ -1,25 +1,42 @@
 from rest_framework.serializers import (
-    ModelSerializer,
+    Serializer,
     IntegerField,
     CharField,
+    DateTimeField,
+    BooleanField,
 )
-from courses.models import Course
 from .author_serializer import AuthorSerializer
 from .type_serializer import CourseTypeSerializer
 
 
-class CourseSerializer(ModelSerializer):
+class CourseSerializer(Serializer):
     """
     Сериализатор курса
 
     """
 
-    owner_id = IntegerField(source="onwer.ref_id")
+    name = CharField()
+    created_at = DateTimeField()
+    updated_at = DateTimeField()
+    owner_id = IntegerField(source="owner.id")
     owner_name = CharField(source="owner.name")
+    thumb_url = CharField(allow_blank=True)
+    cover_url = CharField(allow_blank=True)
+    description = CharField()
+    last_activity = CharField(allow_blank=True)
+    total_score = IntegerField()
+    total_tasks = IntegerField()
+    unchangeable = BooleanField()
+    include_weekly_report = BooleanField()
+    content_type = IntegerField()
+    is_netology = BooleanField()
+    bg_url = CharField()
+    video_url = CharField()
+    demo = BooleanField()
+    custom_author_names = CharField()
+    custom_contents_link = CharField()
+    hide_viewer_navigation = BooleanField()
+    duration = IntegerField()
     authors = AuthorSerializer(many=True)
     types = CourseTypeSerializer(many=True)
 
-    class Meta:
-        model = Course
-        fields = ("__all__", "owner_id", "owner_name", "types", "authors")
-        exclude = ("owner",)
